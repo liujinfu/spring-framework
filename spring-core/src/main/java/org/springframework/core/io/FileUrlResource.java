a2e612b9e478e84c3867516828017918e5ec398d
Jason <dignet1979@gmail.com>
2019-09-02 18:07:56 +0800

    学习Spring源码中的统一资源定位：ResourceLoader，并对相关文件进行注释。

--- spring-core/src/main/java/org/springframework/core/io/FileUrlResource.java
+++ spring-core/src/main/java/org/springframework/core/io/FileUrlResource.java
@@ -29,7 +29,6 @@ import java.nio.file.StandardOpenOption;
 import org.springframework.lang.Nullable;
 import org.springframework.util.ResourceUtils;
 
-
 /**
  * Subclass of {@link UrlResource} which assumes file resolution, to the degree
  * of implementing the {@link WritableResource} interface for it. This resource
